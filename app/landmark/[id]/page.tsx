a1aa4f98bc322f6c8f9cf5d83ee251c964b61ce3
seaskysix <naratorn.kumankad@email.com>
2025-02-26 19:30:55 +0700

    Update

--- app/landmark/[id]/page.tsx
+++ app/landmark/[id]/page.tsx
@@ -5,29 +5,19 @@ import Description from "@/components/landmark/Description";
 import ImageContainer from "@/components/landmark/ImageContainer";
 import ShareButton from "@/components/landmark/ShareButton";
 import MapLandmark from "@/components/map/MapLandmark";
-import { notFound } from "next/navigation";
+import { redirect } from "next/navigation";
 
-// Define the type of the props correctly
-interface LandmarkPageProps {
-  params: {
-    id: string;
-  };
-}
-
-const LandmarkDetail = async ({ params }: LandmarkPageProps) => {
-  const { id } = params; // Ensure this matches the structure of the params field
+// rafce
+const LandmarkDetail = async ({ params }: { params: { id: string } }) => {
+  const { id } = await params;
   const landmark = await fetchLandmarkDetail({ id });
-
-  // If no landmark is found, show a 404 page
-  if (!landmark) {
-    notFound(); // Use Next.js's notFound to display a 404 page
-  }
-
+  if (!landmark) redirect("/");
+  // console.log(landmark);
   return (
     <section>
       <Breadcrums name={landmark.name} />
       <header className="flex justify-between mt-4 items-center">
-        <h1 className="text-4xl font-bold">{landmark.name}</h1>
+        <h1 className="text-4xl font-bold"> {landmark.name}</h1>
         <div className="flex items-center gap-x-4">
           <ShareButton landmarkId={landmark.id} name={landmark.name} />
           <FavoriteToggleButton landmarkId={landmark.id} />
@@ -39,13 +29,12 @@ const LandmarkDetail = async ({ params }: LandmarkPageProps) => {
       <section>
         <div>
           <Description description={landmark.description} />
-          <MapLandmark 
-            location={{ lat: landmark.lat, lng: landmark.lng }} 
+          <MapLandmark
+            location={{ lat: landmark.lat, lng: landmark.lng }}
           />
         </div>
       </section>
     </section>
   );
 };
-
 export default LandmarkDetail;
