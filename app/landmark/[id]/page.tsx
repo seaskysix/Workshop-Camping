b90b097fef3a6032892771742234bc93735ce11c
seaskysix <naratorn.kumankad@email.com>
2025-02-26 22:40:32 +0700

    อัปเดตโค้ดเวอร์ชันใหม่

--- app/landmark/[id]/page.tsx
+++ app/landmark/[id]/page.tsx
@@ -1,48 +1,40 @@
-import React from "react";
 import { fetchLandmarkDetail } from "@/actions/actions";
 import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
 import Breadcrums from "@/components/landmark/Breadcrums";
 import Description from "@/components/landmark/Description";
 import ImageContainer from "@/components/landmark/ImageContainer";
 import ShareButton from "@/components/landmark/ShareButton";
-import { notFound } from "next/navigation";  // ใช้ notFound แทน redirect
-import { GetServerSidePropsContext } from "next"; // Import GetServerSidePropsContext
+import MapLandmark from "@/components/map/MapLandmark";
+import { redirect } from "next/navigation";
 
-const LandmarkDetail = async ({ params }: GetServerSidePropsContext) => {
-  if (!params || typeof params.id !== 'string') {
-    notFound(); // ถ้าไม่พบข้อมูลให้ใช้ notFound()
-    return;
-  }
-
-  const { id } = params;
-
-  // ดึงข้อมูลจาก server โดยตรง
-  const landmarkData = await fetchLandmarkDetail({ id });
-
-  if (!landmarkData) {
-    notFound(); // ถ้าไม่พบข้อมูลให้ใช้ notFound()
-  }
-
+// rafce
+const LandmarkDetail = async ({ params }: { params: { id: string } }) => {
+  const { id } = await params;
+  const landmark = await fetchLandmarkDetail({ id });
+  if (!landmark) redirect("/");
+  // console.log(landmark);
   return (
     <section>
-      <Breadcrums name={landmarkData.name} />
+      <Breadcrums name={landmark.name} />
       <header className="flex justify-between mt-4 items-center">
-        <h1 className="text-4xl font-bold">{landmarkData.name}</h1>
+        <h1 className="text-4xl font-bold"> {landmark.name}</h1>
         <div className="flex items-center gap-x-4">
-          <ShareButton landmarkId={landmarkData.id} name={landmarkData.name} />
-          <FavoriteToggleButton landmarkId={landmarkData.id} />
+          <ShareButton landmarkId={landmark.id} name={landmark.name} />
+          <FavoriteToggleButton landmarkId={landmark.id} />
         </div>
       </header>
       {/* Image */}
-      <ImageContainer mainImage={landmarkData.image} name={landmarkData.name} />
+      <ImageContainer mainImage={landmark.image} name={landmark.name} />
       {/* Detail */}
       <section>
         <div>
-          <Description description={landmarkData.description} />
+          <Description description={landmark.description} />
+          <MapLandmark
+            location={{ lat: landmark.lat, lng: landmark.lng }}
+          />
         </div>
       </section>
     </section>
   );
 };
-
 export default LandmarkDetail;
